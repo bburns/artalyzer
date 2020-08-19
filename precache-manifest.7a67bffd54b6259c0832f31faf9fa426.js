1985f32fbe73f4a95f073ccc06ab597882543e3a
Brian Burns <bburns.km@gmail.com>
2020-08-19 11:44:32 -0500

    Updates

---
+++ precache-manifest.7a67bffd54b6259c0832f31faf9fa426.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f28c58dcb5b5e9a0e8489bfabde1409",
+    "url": "/artalyzer/index.html"
+  },
+  {
+    "revision": "2d46f32491bc1d739de4",
+    "url": "/artalyzer/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b51409e08f2f85cc0da3",
+    "url": "/artalyzer/static/js/2.908c23ff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/artalyzer/static/js/2.908c23ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d46f32491bc1d739de4",
+    "url": "/artalyzer/static/js/main.ac41f13b.chunk.js"
+  },
+  {
+    "revision": "8e7a6287345dc1cb9607",
+    "url": "/artalyzer/static/js/runtime-main.de04d314.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/artalyzer/static/media/logo.5d5d9eef.svg"
+  }
+]);
